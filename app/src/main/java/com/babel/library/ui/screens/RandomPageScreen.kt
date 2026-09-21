104da77b03c670e990165edcb5d733d4e58df940
alibahsburiev20-cyber <alibahsburiev20@gmail.com>
2026-09-21 10:23:48 +0300

    Update RandomPageScreen.kt

--- app/src/main/java/com/babel/library/ui/screens/RandomPageScreen.kt
+++ app/src/main/java/com/babel/library/ui/screens/RandomPageScreen.kt
@@ -18,7 +18,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.rotate
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.babel.library.core.Address
